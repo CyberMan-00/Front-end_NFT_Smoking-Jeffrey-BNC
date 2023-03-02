c7d0c376cb6b788e769eaa9ac6e912910811c60d
Miran Shamaev <61889214+CyberMan-00@users.noreply.github.com>
2023-03-02 17:58:32 +0100

    optimization + side svgs

--- dist/js/rain.js
+++ dist/js/rain.js
@@ -2,8 +2,8 @@ let hrElement;
 let counter = 100;
 for (let i = 0; i < counter; i++) {
     hrElement = document.createElement("HR");
-    hrElement.style.right = Math.floor(Math.random() * window.innerWidth) + "px";
-    hrElement.style.animationDuration = 3 + Math.random() * 0.3 + "s";
+    hrElement.style.right = Math.floor(Math.random() * window.innerWidth * 1) + "px";
+    hrElement.style.animationDuration = 5 + Math.random() * 0.3 + "s";
     hrElement.style.animationDelay = Math.random() * 10 + "s";
     document.body.appendChild(hrElement);
 }
